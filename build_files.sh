6a13db5092c8e22ed9bdac473fbd2c2460e3bc67
mirza ali <84387676+bilalmirza74@users.noreply.github.com>
2023-09-27 20:42:21 +0530

    .

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo "BUILD START"
-pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
+pip install --upgrade pip
 echo "BUILD END"
