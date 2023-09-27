2aa3ea99cca025ce9b70414143eb328cb02e5ec4
mirza ali <84387676+bilalmirza74@users.noreply.github.com>
2023-09-27 16:12:41 +0000

    .

--- PresagingTechnique/settings.py
+++ PresagingTechnique/settings.py
@@ -128,3 +128,4 @@ STATICFILES_DIRS = os.path.join(BASE_DIR, 'assets/static'),
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
